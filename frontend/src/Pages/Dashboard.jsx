6bdb0b16b173fca041fd8012de8d68a372075e25
Kanwar Muzammil Rohail <muzzammil606@gmail.com>
2023-12-27 02:49:19 +0500

    Dashboard css

--- frontend/src/Pages/Dashboard.jsx
+++ frontend/src/Pages/Dashboard.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
+import './Dashboard.css';
 
 const Dashboard = () => {
   const { userId } = useParams();
