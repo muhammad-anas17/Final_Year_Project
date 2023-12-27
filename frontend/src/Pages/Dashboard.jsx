3617b2a692b9f150d92ccb96516f6420d5c86f2e
Kanwar Muzammil Rohail <muzzammil606@gmail.com>
2023-12-28 02:17:38 +0500

    Update Dashboard.jsx

--- frontend/src/Pages/Dashboard.jsx
+++ frontend/src/Pages/Dashboard.jsx
@@ -135,7 +135,7 @@ const Dashboard = () => {
       <div className="container mt-5">
         <h1 className="text-center">Welcome to Your Dashboard</h1>
         {userData ? (
-          <div className="user-info card p-4">
+          <div className="user-info card p-4 mb-4">
             <p className="lead">User ID: {userData.id}</p>
             <p className="lead">Email: {userData.email}</p>
           </div>
@@ -143,36 +143,39 @@ const Dashboard = () => {
           <p>Loading user data...</p>
         )}
 
-        <div className="colleges">
+        <div className="row row-cols-1 row-cols-md-2 g-4">
           {colleges.map((college) => (
-            <div className="college card p-4" key={college.id}>
-              <h3 className="mb-3">College</h3>
-              <p className="lead">Name: {college.name}</p>
-              <p className="lead">ID: {college.id}</p>
+            <div className="col" key={college.id}>
+              <div className="college card p-4">
+                <h3 className="mb-3">College</h3>
+                <p className="lead">Name: {college.name}</p>
+                <p className="lead">ID: {college.id}</p>
 
-              {deleteCollegeId === college.id ? (
-                <button className="btn btn-danger" onClick={handleDelete}>
-                  Withdraw
-                </button>
-              ) : (
-                <div className="form-check mb-3">
-                  <input
-                    className="form-check-input"
-                    type="checkbox"
-                    onChange={() => handleCheckboxChange(college.id)}
-                    checked={selectedColleges.includes(college.id)}
-                  />
-                  <label className="form-check-label">Apply</label>
-                </div>
-              )}
+                {deleteCollegeId === college.id ? (
+                  <button className="btn btn-danger" onClick={handleDelete}>
+                    Withdraw
+                  </button>
+                ) : (
+                  <div className="form-check mb-3">
+                    <input
+                      className="form-check-input"
+                      type="checkbox"
+                      onChange={() => handleCheckboxChange(college.id)}
+                      checked={selectedColleges.includes(college.id)}
+                    />
+                    <label className="form-check-label">Apply</label>
+                  </div>
+                )}
+              </div>
             </div>
           ))}
-          {selectedColleges.length > 0 && (
-            <button className="btn btn-success" onClick={handleApplyAll}>
-              Apply to Selected Colleges
-            </button>
-          )}
         </div>
+
+        {selectedColleges.length > 0 && (
+          <button className="btn btn-success mt-4" onClick={handleApplyAll}>
+            Apply to Selected Colleges
+          </button>
+        )}
       </div>
     </div>
   );
