95fd9caef8bd0865654805ade3ac003ccc2e55f0
Ammar Amin <97978396+theammaramin@users.noreply.github.com>
2023-12-28 01:52:15 +0500

    applicant

--- frontend/src/Pages/Applicant.jsx
+++ frontend/src/Pages/Applicant.jsx
@@ -3,25 +3,20 @@ import axios from 'axios';
 import { useParams } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Navbar2 from '../components/Navbar2'
+import Navbar2 from '../components/Navbar2';
 
 const Applicant = () => {
   const navigate = useNavigate();
   const { userId } = useParams();
   const [applicantData, setApplicantData] = useState(null);
-  const [gradesData, setGradesData] = useState(null);
-  
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await axios.get(`http://localhost:8800/api/status/${userId}`);
         setApplicantData(response.data);
-        const user = response.data.UserId;
-        console.log("Applicant ID: ", user);
-        const res= await axios.get(`http://localhost:8800/api/grades/${user}`);
-        console.log(res);
-        
+
+        console.log(response.data);
       } catch (error) {
         console.error('Error fetching applicant data:', error);
       }
@@ -32,16 +27,30 @@ const Applicant = () => {
 
   return (
     <div>
-      <header><Navbar2/></header>
+      <header>
+        <Navbar2 />
+      </header>
       <h2>Applicant Information</h2>
       {applicantData ? (
-
-        <div>
-          <p>StatusID: {applicantData.StatusID}</p>
-          <p>UserId: {applicantData.UserId}</p>
-          <p>SubmissionDate: {applicantData.SubmissionDate}</p>
-          <p>ReviewStatus: {applicantData.ReviewStatus}</p>
-        </div>
+        Array.isArray(applicantData) ? (
+          applicantData.map((record) => (
+            <div key={record.StatusID}>
+              <p>StatusID: {record.StatusID}</p>
+              <p>UserId: {record.UserId}</p>
+              <p>SubmissionDate: {record.SubmissionDate}</p>
+              <p>ReviewStatus: {record.ReviewStatus}</p>
+              <hr />
+            </div>
+          ))
+        ) : (
+          <div>
+            <p>StatusID: {applicantData.StatusID}</p>
+            <p>UserId: {applicantData.UserId}</p>
+            <p>SubmissionDate: {applicantData.SubmissionDate}</p>
+            <p>ReviewStatus: {applicantData.ReviewStatus}</p>
+            
+          </div>
+        )
       ) : (
         <p>Loading...</p>
       )}
